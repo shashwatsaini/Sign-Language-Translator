82d7c6d1d663ed02313ad5efb24477c4ce0e229e
Shashwat Saini <125039068+shashwatsaini@users.noreply.github.com>
2023-06-22 14:26:31 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,290 @@
+import sys
+import cv2
+import mediapipe as mp
+import pandas as pd
+import numpy as np
+import math
+import tensorflow as tf
+from tensorflow import lite
+from PyQt5.QtGui import QImage, QPixmap, QIcon, QFont
+from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QVBoxLayout, QWidget, QLineEdit, QPushButton, QHBoxLayout, QDialog, QMenuBar, QAction, QMessageBox
+
+from bin.aboutApp import aboutApp
+from bin.help import Help
+
+classes = [chr(i) for i in range(ord('A'), ord('Z') + 1) if chr(i) not in ['J', 'Z']]
+classes.append('_')
+interpreter = tf.lite.Interpreter(model_path='models/model.tflite')
+interpreter.allocate_tensors()
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(max_num_hands=1)
+mpDraw = mp.solutions.drawing_utils
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setWindowTitle("Sign Language Translator")
+
+        font = QFont()
+        font.setPointSize(8)
+        QApplication.setFont(font)
+
+        self.icon = QIcon('icons/main.png')
+        self.setWindowIcon(self.icon)
+
+        # Some variables that will be used through the program
+        self.processed_frames = pd.DataFrame(columns=[str(i) for i in range(25)] + ['label']) # Saves the processed characters which can improve the model
+        self.text_field_stream = ''
+        self.repeat_character_timer = 0     # Used when a character is repeating
+        self.confidence_character = 0       # Used to ensure a random character prediction is not included, such as when transitioning between characters
+
+        self.central_widget = QWidget(self)
+        self.setCentralWidget(self.central_widget)
+
+        self.image_label = QLabel(self)
+        layout = QVBoxLayout(self.central_widget)
+        layout.addWidget(self.image_label)
+
+        self.text_field = QLineEdit()
+        self.text_field.setPlaceholderText('Text will be display here')
+        layout.addWidget(self.text_field)
+
+        self.clear_button = QPushButton('Clear')
+        self.clear_button.clicked.connect(self.clearTextField)
+        layout.addWidget(self.clear_button)
+
+        self.label1 = QLabel('Was this response correct?')
+        self.response_button_yes = QPushButton('Yes')
+        self.response_button_yes.clicked.connect(self.saveProcessedFields)
+        self.response_button_no = QPushButton('No')
+        self.response_button_no.clicked.connect(self.discardProcessedFields)
+
+        self.response_layout = QHBoxLayout()
+        self.response_layout.addWidget(self.label1)
+        self.response_layout.addWidget(self.response_button_yes)
+        self.response_layout.addWidget(self.response_button_no)
+
+        self.menubar = QMenuBar()
+        self.model_list = self.menubar.addMenu('Model')
+        self.update_model_option = QAction('Update Model', self)
+        self.update_model_option.triggered.connect(self.update_model)
+        self.model_list.addAction(self.update_model_option)
+
+        self.train_model_option = QAction('Train this model', self)
+        self.train_model_option.triggered.connect(self.train_model_clicked)
+        self.model_list.addAction(self.train_model_option)
+
+        self.help_list = self.menubar.addMenu('Help')
+        self.about_option = QAction('About this App', self)
+        self.about_option.triggered.connect(self.about)
+        self.help_list.addAction(self.about_option)
+
+        self.how_to_use_option = QAction('How to use this application?', self)
+        self.how_to_use_option.triggered.connect(self.how_to_use)
+        self.help_list.addAction(self.how_to_use_option)
+
+        layout.setMenuBar(self.menubar)
+        layout.addLayout(self.response_layout)
+
+        self.cap = cv2.VideoCapture(0)
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.process_frame)
+        self.timer.start(100)
+
+    def closeEvent(self, event):
+        self.timer.stop()
+        super(MainWindow, self).closeEvent(event)
+
+    def clearTextField(self):
+        self.text_field.setText('')
+        self.text_field_stream = ''
+
+    def saveProcessedFields(self):
+        try:
+            file = pd.read_csv('data/update_model.csv')
+            self.processed_frames = self.processed_frames[file.columns]
+            file = pd.concat([file, self.processed_frames], axis=0)
+            file.to_csv('data/update_model.csv', index=False)
+        except FileNotFoundError:
+            columns = [str(i) for i in range(25)]+['label']
+            file = pd.DataFrame(columns=columns)
+            file = pd.concat([file, self.processed_frames], axis=0, ignore_index=True)
+            file.to_csv('data/update_model.csv', index=False)
+        except Exception as e:
+            print(e)
+        temp = pd.DataFrame(columns=[str(i) for i in range(25)] + ['label'])
+        self.processed_frames = temp.copy()
+
+    def update_model(self):
+
+        train = pd.read_csv('data/update_model.csv')
+
+        if train['label'].nunique() > 0:
+            X_train = train.copy()
+            train_y = X_train.pop('label')
+            # Displaying a basic alert
+            alert = QMessageBox()
+            alert.setIcon(QMessageBox.Information)
+            alert.setText("The system is retraining the model. This is likely to cause lag.")
+            alert.setWindowTitle("Alert")
+            alert.setStandardButtons(QMessageBox.Ok)
+            alert.exec_()
+
+            from sklearn.preprocessing import OrdinalEncoder
+            oe = OrdinalEncoder()
+            train_y = oe.fit_transform(train_y.to_numpy().reshape(-1, 1))
+            model = tf.keras.models.load_model('models/model.h5')
+            model.fit(X_train, train_y, epochs=10)
+
+            # Save the new model
+            tf.keras.models.save_model(model, 'models/model.h5')
+
+            # Save the TensorFlow Lite model
+            converter = tf.lite.TFLiteConverter.from_keras_model(model)
+            tflite_model = converter.convert()
+            with open('models/model.tflite', 'wb') as f:
+                f.write(tflite_model)
+
+            # Reloading the model
+            interpreter = tf.lite.Interpreter(model_path='models/model.tflite')
+            interpreter.allocate_tensors()
+
+            # Deleting update_model.csv
+            train.drop(train.index, inplace=True)
+            train.to_csv('data/update_model.csv', index=False)
+
+            del train, X_train, train_y, model
+        else:
+            alert = QMessageBox()
+            alert.setIcon(QMessageBox.Information)
+            alert.setText("New data was not found to retrain the model..")
+            alert.setWindowTitle("Alert")
+            alert.setStandardButtons(QMessageBox.Ok)
+            alert.exec_()
+
+            del train
+
+    def train_model_clicked(self):
+        alert = QMessageBox()
+        alert.setIcon(QMessageBox.Information)
+        alert.setText("Please open the 'Train Model' file to train the model.")
+        alert.setWindowTitle("Alert")
+        alert.setStandardButtons(QMessageBox.Ok)
+        alert.exec_()
+
+    def discardProcessedFields(self):
+        temp = pd.DataFrame(columns=[str(i) for i in range(25)] + ['label'])
+        self.processed_frames = temp.copy()
+
+    def normalize(self, entry):
+        ref = (entry.loc[0, '0_x'], entry.loc[0, '0_y'])
+
+        hand_points = []
+        for i in range(0, 21):
+            hand_points.append((entry.loc[0, str(i) + '_x'], entry.loc[0, str(i) + '_y']))
+
+        distances = []
+        for point in hand_points:
+            distance = math.sqrt((point[0] - ref[0]) ** 2 + (point[1] - ref[1]) ** 2)
+            distances.append(distance)
+
+        norm_distances = []
+        ref2 = hand_points[12]
+        palm_distance = math.sqrt((ref[0] - ref2[0]) ** 2 + (ref[1] - ref2[1]) ** 2)
+        finger_distance = math.sqrt(
+            (hand_points[8][0] - hand_points[12][0]) ** 2 + (hand_points[8][1] - hand_points[12][1]) ** 2)
+        k1 = math.sqrt((hand_points[5][0] - hand_points[9][0]) ** 2 + (hand_points[5][1] - hand_points[9][1]) ** 2)
+        k2 = math.sqrt((hand_points[13][0] - hand_points[17][0]) ** 2 + (hand_points[13][1] - hand_points[17][1]) ** 2)
+        k3 = math.sqrt((hand_points[9][0] - hand_points[13][0]) ** 2 + (hand_points[9][1] - hand_points[13][1]) ** 2)
+        for distance in distances:
+            norm_distance = distance / palm_distance
+            norm_distances.append(norm_distance)
+
+        norm_distances.append(finger_distance / palm_distance)
+        norm_distances.append(k1 / palm_distance)
+        norm_distances.append(k2 / palm_distance)
+        norm_distances.append(k3 / palm_distance)
+
+        entry_final = pd.DataFrame()
+
+        for i in range(0, 25):
+            entry_final.loc[0, str(i)] = norm_distances[i]
+
+        return entry_final
+
+    def process_frame(self):
+        success, img = self.cap.read()
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = hands.process(imgRGB)
+
+        if results.multi_hand_landmarks:
+            test = pd.DataFrame()
+            for handVisible in results.multi_hand_landmarks:
+                for id, lm in enumerate(handVisible.landmark):
+                    h, w, c = img.shape
+                    cx, cy = int(lm.x * w), int(lm.y * h)
+                    test.loc[0, str(id) + '_x'] = cx
+                    test.loc[0, str(id) + '_y'] = cy
+
+                mpDraw.draw_landmarks(img, handVisible, mpHands.HAND_CONNECTIONS)
+            # Normalizing:
+            test_final = self.normalize(test)
+            input_data = np.array(test_final)
+            input_data = input_data.astype(np.float32)
+            interpreter.set_tensor(interpreter.get_input_details()[0]['index'], input_data)
+            interpreter.invoke()
+            output_data = interpreter.get_tensor(interpreter.get_output_details()[0]['index'])
+            y_preds = tf.nn.softmax(output_data)
+            if y_preds[0][np.argmax(y_preds)] > 0.0:    # Change this condition
+                pred = classes[np.argmax(y_preds)]
+                stream = pred
+
+                if self.text_field_stream and self.text_field_stream[-1] == stream:
+                    self.repeat_character_timer += 1
+                elif self.repeat_character_timer > 10:
+                    test_final['label'] = pred
+                    self.processed_frames = pd.concat([self.processed_frames, test_final], axis=0, ignore_index=True)
+                    if pred == '_':
+                        self.text_field_stream += ' '
+                    else:
+                        self.text_field_stream += stream
+                    self.text_field.setText(self.text_field_stream)
+                    self.repeat_character_timer = 0
+                else:
+                    self.confidence_character += 1
+                    if self.confidence_character > 10:
+                        test_final['label'] = pred
+                        self.processed_frames = pd.concat([self.processed_frames, test_final], axis=0, ignore_index=True)
+                        self.confidence_character = 0
+                        if pred == '_':
+                            self.text_field_stream += ' '
+                        else:
+                            self.text_field_stream += stream
+                        self.text_field.setText(self.text_field_stream)
+                        self.repeat_character_timer = 0
+
+        height, width, channel = img.shape
+        bytes_per_line = channel * width
+        q_image = QImage(img.data, width, height, bytes_per_line, QImage.Format_RGB888)
+        pixmap = QPixmap.fromImage(q_image)
+        self.image_label.setPixmap(pixmap)
+        self.image_label.setScaledContents(True)
+
+    def about(self):
+        self.temp_window = aboutApp()
+        self.temp_window.show()
+
+    def how_to_use(self):
+        self.temp_window = Help()
+        self.temp_window.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
---
+++ trainModel.py
@@ -0,0 +1,200 @@
+import sys
+import cv2
+import mediapipe as mp
+import pandas as pd
+import numpy as np
+import math
+import tensorflow as tf
+from tensorflow import lite
+from PyQt5.QtGui import QImage, QPixmap, QIcon, QFont
+from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QVBoxLayout, QWidget, QLineEdit, QPushButton, QHBoxLayout, QDialog, QMenuBar, QAction, QMessageBox
+
+
+class TrainModel(QMainWindow):
+
+    def __init__(self):
+        super(TrainModel, self).__init__()
+
+        font = QFont()
+        font.setPointSize(8)
+        QApplication.setFont(font)
+
+        self.classes = [chr(i) for i in range(ord('A'), ord('Z') + 1) if chr(i) not in ['J', 'Z']]
+        self.classes.append('_')
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(max_num_hands=1)
+        self.mpDraw = mp.solutions.drawing_utils
+
+        self.setWindowTitle("Train Model")
+        self.setWindowIcon(QIcon('icons/main.png'))
+
+        self.train = pd.DataFrame(columns=[str(i) for i in range(25)] + ['label'])  # Main dataframe to store the train data
+        self.label = '_'  # Label for the current letter
+        self.label_index = 0   # Index of the current letter in the classes list
+        self.iterator = 0  # Iterator to keep track of the number of frames processed for each letter
+
+        self.central_widget = QWidget(self)
+        self.setCentralWidget(self.central_widget)
+
+        self.image_label = QLabel(self)
+        layout = QVBoxLayout(self.central_widget)
+        layout.addWidget(self.image_label)
+
+        self.training_layout = QHBoxLayout()
+        self.text1 = QLabel('Sign this character: ')
+        self.label_button = QPushButton(str(self.label))
+        self.training_layout.addWidget(self.text1)
+        self.training_layout.addWidget(self.label_button)
+        layout.addLayout(self.training_layout)
+
+        self.interface_layout = QHBoxLayout()
+        self.cancel_button = QPushButton('Cancel')
+        self.cancel_button.clicked.connect(self.cancel_clicked)
+        self.interface_layout.addWidget(self.cancel_button)
+        layout.addLayout(self.interface_layout)
+
+        self.cap = cv2.VideoCapture(0)
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.process_frame)
+        self.timer.start(100)
+
+    def normalize(self, entry):
+        ref = (entry.loc[0, '0_x'], entry.loc[0, '0_y'])
+
+        hand_points = []
+        for i in range(0, 21):
+            hand_points.append((entry.loc[0, str(i) + '_x'], entry.loc[0, str(i) + '_y']))
+
+        distances = []
+        for point in hand_points:
+            distance = math.sqrt((point[0] - ref[0]) ** 2 + (point[1] - ref[1]) ** 2)
+            distances.append(distance)
+
+        norm_distances = []
+        ref2 = hand_points[12]
+        palm_distance = math.sqrt((ref[0] - ref2[0]) ** 2 + (ref[1] - ref2[1]) ** 2)
+        finger_distance = math.sqrt(
+            (hand_points[8][0] - hand_points[12][0]) ** 2 + (hand_points[8][1] - hand_points[12][1]) ** 2)
+        k1 = math.sqrt((hand_points[5][0] - hand_points[9][0]) ** 2 + (hand_points[5][1] - hand_points[9][1]) ** 2)
+        k2 = math.sqrt((hand_points[13][0] - hand_points[17][0]) ** 2 + (hand_points[13][1] - hand_points[17][1]) ** 2)
+        k3 = math.sqrt((hand_points[9][0] - hand_points[13][0]) ** 2 + (hand_points[9][1] - hand_points[13][1]) ** 2)
+        for distance in distances:
+            norm_distance = distance / palm_distance
+            norm_distances.append(norm_distance)
+
+        norm_distances.append(finger_distance / palm_distance)
+        norm_distances.append(k1 / palm_distance)
+        norm_distances.append(k2 / palm_distance)
+        norm_distances.append(k3 / palm_distance)
+
+        entry_final = pd.DataFrame()
+
+        for i in range(0, 25):
+            entry_final.loc[0, str(i)] = norm_distances[i]
+
+        return entry_final
+
+    def train_model(self):
+        # Displaying a basic alert
+        alert = QMessageBox()
+        alert.setIcon(QMessageBox.Information)
+        alert.setText("The system is training the model. This is likely to cause lag.")
+        alert.setWindowTitle("Alert")
+        alert.setStandardButtons(QMessageBox.Ok)
+        alert.exec_()
+
+        train = pd.read_csv('data/train.csv')
+        X = train.drop('label', axis=1)
+        y = train['label']
+
+        from sklearn.preprocessing import OrdinalEncoder
+        oe = OrdinalEncoder()
+        y = oe.fit_transform(y.to_numpy().reshape(-1, 1))
+
+        model = tf.keras.models.Sequential([
+            tf.keras.layers.Dense(64, input_dim=X.shape[1], activation='relu'),
+            tf.keras.layers.Dense(128, activation='relu'),
+            tf.keras.layers.Dense(25, activation='softmax')
+        ])
+
+        model.compile(optimizer='adam',
+                      loss='sparse_categorical_crossentropy',
+                      metrics=['accuracy'])
+
+        model.fit(X, y, epochs=100)
+
+        model.save('models/model.h5')
+
+        converter = lite.TFLiteConverter.from_keras_model(model)
+        tflite_model = converter.convert()
+        open('models/model.tflite', 'wb').write(tflite_model)
+
+        alert = QMessageBox()
+        alert.setIcon(QMessageBox.Information)
+        alert.setText("Model trained successfully. This window will now close.")
+        alert.setWindowTitle("Alert")
+        alert.setStandardButtons(QMessageBox.Ok)
+        alert.exec_()
+
+        sys.exit(0)
+
+    def process_frame(self):
+        success, img = self.cap.read()
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = self.hands.process(imgRGB)
+
+        if self.iterator < 50 and self.label_index < 25:
+            self.label = self.classes[self.label_index]
+            self.label_button.setText(str(self.label))
+            if results.multi_hand_landmarks:
+                train = pd.DataFrame()
+                for handVisible in results.multi_hand_landmarks:
+                    for id, lm in enumerate(handVisible.landmark):
+                        h, w, c = img.shape
+                        cx, cy = int(lm.x * w), int(lm.y * h)
+                        train.loc[0, str(id) + '_x'] = cx
+                        train.loc[0, str(id) + '_y'] = cy
+
+                    self.mpDraw.draw_landmarks(img, handVisible, self.mpHands.HAND_CONNECTIONS)
+
+                # Normalizing
+                train_final = self.normalize(train)
+                train_final['label'] = self.label
+                self.train = pd.concat([self.train, train_final], ignore_index=True)
+
+                self.iterator += 1
+
+        else:
+            self.iterator = 0
+            self.label_index += 1
+            self.train.to_csv('data/train.csv', index=False)
+            try:    # For when the label_index exceeds the number of classes, when training is done
+                self.label = self.classes[self.label_index]
+                alert = QMessageBox()
+                alert.setIcon(QMessageBox.Information)
+                alert.setText("Sign this Character: " + str(self.label))
+                alert.setWindowTitle("Alert")
+                alert.setStandardButtons(QMessageBox.Ok)
+                alert.exec_()
+            except:
+                self.train_model()
+
+        height, width, channel = img.shape
+        bytes_per_line = channel * width
+        q_image = QImage(img.data, width, height, bytes_per_line, QImage.Format_RGB888)
+        pixmap = QPixmap.fromImage(q_image)
+        self.image_label.setPixmap(pixmap)
+        self.image_label.setScaledContents(True)
+
+    def cancel_clicked(self):
+        self.close()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = TrainModel()
+    window.show()
+    sys.exit(app.exec_())
